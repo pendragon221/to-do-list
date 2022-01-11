45d967bb7c938f9a32d9fb4b5a98024cca03d6dd
Pendragon <44371239+pendragon221@users.noreply.github.com>
2022-01-11 16:06:00 +0300

    Added animation

--- src/components/TasksList/TasksList.jsx
+++ src/components/TasksList/TasksList.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { CSSTransition, TransitionGroup } from 'react-transition-group'
 import Task from '../Task/Task'
 import styles from './TasksList.module.css'
 
@@ -8,14 +9,29 @@ const TasksList = ({ tasks, setTasks }) => {
         setTasks(tasks.filter((el) => el.id !== post.id))
     }
 
-    return (
-        <div className={styles.tasksList}>
-            <h1>All tasks</h1>
-            {tasks.map((el, index) => (
-                <Task task={el} number={index + 1} remove={onDelete} key={el.id} />
-            ))}
-        </div>
-    )
+    if (!tasks.length)
+        return (
+            <div className={styles.tasksList}>
+                <h1>No tasks</h1>
+            </div>
+        )
+    else {
+        return (
+            <div className={styles.tasksList}>
+                <h1>All tasks</h1>
+                <TransitionGroup>
+                    {tasks.map((el, index) => (
+                        <CSSTransition
+                            key={el.id}
+                            timeout={500}
+                            classNames="task">
+                            <Task task={el} number={index + 1} remove={onDelete} />
+                        </CSSTransition>
+                    ))}
+                </TransitionGroup>
+            </div>
+        )
+    }
 }
 
 export default TasksList
